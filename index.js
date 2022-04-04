96f3dcb7ff031b90bf864af053ec56695e6b8d0d
Sheena Bonilla <sheena.myb@gmail.com>
2022-04-03 22:07:49 -0700

    commit tree 012e8fca08bc91bfbb3be043b177e02d16c3dfbb

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function () {
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers,
+]
+
+const createFareMultiplier = function (num1) {
+    return function (num2) {
+        return num1 * num2;
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(drivers, driversReturned) {
+    return driversReturned(drivers);
+}
